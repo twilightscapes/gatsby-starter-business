03f1332c4d598fd1ec5b870951b872a3dab5aa0f
twilightscapes <ihatespam@gmail.com>
2019-12-23 01:00:57 -0600

    fix?

--- src/templates/article-page.js
+++ src/templates/article-page.js
@@ -4,8 +4,10 @@ import { graphql } from 'gatsby'
 import { HTMLContent } from '../components/Content'
 import ArticleTemplate from '../components/ArticleTemplate'
 import SE0 from '../components/SEO'
+/*
 import Share from '../components/Share'
 import Disqus from '../components/Disqus'
+*/
 import Layout from '../components/Layout'
 
 const ArticlePage = ({ data }) => {
@@ -33,16 +35,7 @@ const ArticlePage = ({ data }) => {
                 tags={post.frontmatter.tags}
                 title={post.frontmatter.title}
               />
-              <Share
-                title={post.frontmatter.title}
-                slug={post.fields.slug}
-                excerpt={post.frontmatter.meta_description}
-              />
-              <hr />
-              <Disqus
-                title={post.frontmatter.title}
-                slug={post.fields.slug}
-              />
+              
             </div>
           </div>
         </div>
